00e96f6afbf311fdd78eb576784b7d3c27c9ecf6
mateoroca <mateojoserocaclemntis@gmail.com>
2023-09-01 20:01:08 -0300

    se creo el web component en el front y se realizo la gestion del envio desde el front y la respuesta del back end para la carga de varios archivos al sevidor

--- front-end/web-components/x-File-Uploader/controller/controllerFileUploader.js
+++ front-end/web-components/x-File-Uploader/controller/controllerFileUploader.js
@@ -5,19 +5,38 @@ class FileUploaderController {
   }
 
   enable() {
-    this.view.button2.addEventListener("click", (e) => {
+    this.view.BtnSendFile.addEventListener("click", (e) => {
       e.preventDefault();
-    });
-
-    this.view.button3.addEventListener("click", (e) => {
-      e.preventDefault();
-      this.onbuttomForgotPasswordClick();
+      this.onButtonBtnSendFile();
     });
   }
 
   disable() {
     this.view.btnSignUp = null;
     this.view.btnForgotPassw = null;
+  }
+  async onButtonBtnSendFile() {
+    let fileInput = this.view.fileInput.files;
+    console.log(Object.keys(fileInput));
+    const formData = new FormData();
+
+    Object.keys(fileInput).forEach((k) => {
+      formData.append("file", fileInput[k]);
+    });
+
+    try {
+      let requestMetadata = {
+        method: "POST",
+        body: formData,
+      };
+
+      let res = await fetch("http://localhost:3000/upload", requestMetadata);
+      let jsonBody = await res.json();
+      alert(jsonBody.message);
+      return res;
+    } catch (error) {
+      alert(error.message);
+    }
   }
 }
 
