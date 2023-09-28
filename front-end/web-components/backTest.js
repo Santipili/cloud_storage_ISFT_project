a9513e3daa69ee13899524cf103c5d38a374fbbe
Santipili <santi_mdq@live.com.ar>
2023-09-28 17:30:28 -0300

    implemente el borrado de un directerio en concreto

--- front-end/web-components/backTest.js
+++ front-end/web-components/backTest.js
@@ -5,12 +5,17 @@ class BackTest extends HTMLElement {
         super();
         this.container = document.createElement('div');
         this.appendChild(this.container);
-        this.button = createElement('button', 'ENVIAR');
+        this.buttonC = createElement('button', 'Crear');
+        this.buttonD = createElement('button', 'Borrar');
     }
     connectedCallback() {
-        this.container.appendChild(this.button); 
-        this.button.addEventListener('click', ()=> {
+        this.container.appendChild(this.buttonC); 
+        this.container.appendChild(this.buttonD); 
+        this.buttonC.addEventListener('click', ()=> {
             this.newDirectorie();
+        })
+        this.buttonD.addEventListener('click', ()=> {
+            this.deleteDirectorie();
         })
     }
     disconnectedCallback() {    
@@ -31,6 +36,29 @@ class BackTest extends HTMLElement {
                 body:JSON.stringify(data),
               };
             let result = await fetch ("http://localhost:3000/upload/newfolder", requestMetadata);    
+            let jsonResult = await result.json();
+            alert(jsonResult.message); 
+            console.log(jsonResult);           
+        } catch (error) {
+            console.log("error");         
+            alert(error);
+        }
+    }
+
+    async deleteDirectorie(){
+        let data = {
+            userDir : "/" + "userNameSanti",
+            newDir : "/0012assa0"
+        }
+        try {      
+            let requestMetadata = {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json' 
+                },
+                body:JSON.stringify(data),
+              };
+            let result = await fetch ("http://localhost:3000/upload/deletefolder", requestMetadata);    
             let jsonResult = await result.json();
             alert(jsonResult.message); 
             console.log(jsonResult);           
