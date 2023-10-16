5adabbb9f2457a0de9288827b7da828a7fc7305f
AldoCap <capurro.ac@gmail.com>
2023-10-16 18:30:23 -0300

    trabajos visuales con drag and drop

--- front-end/web-components/x-File-Uploader/view/fileUploaderView.js
+++ front-end/web-components/x-File-Uploader/view/fileUploaderView.js
@@ -34,31 +34,14 @@ class FileUploaderView extends HTMLElement {
     this.BtnSendFile.textContent = "Upload";
     this.BtnSendFile.setAttribute("style", "display: none;");
     this.BtnSendFile.disabled = true;
-
-    /*
-    this.DivProgressBar = document.createElement("div");
-    this.DivProgressBar.classList.add("Div");
-
-    this.progressBar = document.createElement("progress");
-    this.progressBar.setAttribute("max", "100");
-    this.progressBar.setAttribute("value", "0");
-    this.progressBar.setAttribute("style", "display: none;");
-
-    this.progressSpan = document.createElement("span");
-    this.progressSpan.className = "progress-span";
-    this.progressSpan.textContent = "0%";
-    this.progressSpan.setAttribute("style", "display: none;");
-    */
     this.DivBtn.appendChild(this.BtnSendFile);
-    //this.DivProgressBar.appendChild(this.progressSpan);
-    //this.DivProgressBar.appendChild(this.progressBar);
+
 
     this.label.appendChild(this.dropTitle);
     this.label.appendChild(this.fileInput);
     this.form.appendChild(this.Title);
     this.form.appendChild(this.label);
     this.form.appendChild(this.DivBtn);
-    //this.form.appendChild(this.DivProgressBar);
 
     this.appendChild(this.form);
 
@@ -66,21 +49,28 @@ class FileUploaderView extends HTMLElement {
       this.BtnSendFile.disabled = false;
       this.BtnSendFile.setAttribute("style", "display: inline-flex;");
     });
+
+    this.label.addEventListener("dragover", (event) => {
+      event.preventDefault();
+      if (!this.label.classList.contains("drag-over")) {
+        this.label.classList.add("drag-over");
+      }
+    });
+    
+    this.label.addEventListener("dragleave", (event) => {
+      event.preventDefault();
+      this.label.classList.remove("drag-over");
+  ;
+    });
+  
+    this.label.addEventListener("drop", (event) => {
+      this.label.classList.remove("drag-over");
+    });
+    
   }
   connectedCallback() {}
 
   disconnectedCallback() {}
-/*
-  enableProgressBar() {
-    this.progressBar.setAttribute("style", "display: block;");
-    this.progressSpan.setAttribute("style", "display: block;");
-  }
-
-  disableProgressBar() {
-    this.progressBar.setAttribute("style", "display: none;");
-    this.progressSpan.setAttribute("style", "display: none;");
-  }
-*/
 
   getFormData() {
     const fileInput = this.fileInput.files;
@@ -106,12 +96,6 @@ class FileUploaderView extends HTMLElement {
       return res;
     }
   }
-  /*
-  updateProgressBar(percentCompleted) {
-    this.progressBar.value = percentCompleted;
-    this.progressSpan.innerText = `${Math.round(percentCompleted)}%`;
-  }
-  */
 }
 
 customElements.define("file-uploader-view", FileUploaderView);
